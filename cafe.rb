4f8d4bdef1271ee7c6f0f7067ed2b58ac6e641e4
naoki00413 <popoteto0413@shiolab.com>
2023-02-13 09:05:42 +0900

    cafe.rbのバグを修正
    修正箇所
    ・頼んだつもりのメニューが選択できない
    ・最後の番号のメニューを選択するとエラーになる
    ・合計お会計金額が異様に高い
    ・違う番号のドリンク、フードを選択するとお会計が正しくない

--- cafe.rb
+++ cafe.rb
@@ -1,7 +1,6 @@
 # frozen_string_literal: true
-
 DRINKS = [
-  { name: 'コーヒー', price: '300' },
+  { name: 'コーヒー', price: '300' }, 
   { name: 'カフェラテ', price: '400' },
   { name: 'チャイ', price: '460' },
   { name: 'エスプレッソ', price: '340' },
@@ -9,7 +8,7 @@ DRINKS = [
 ].freeze
 
 FOODS = [
-  { name: 'チーズケーキ', price: '470' },
+  { name: 'チーズケーキ', price: '470' }, 
   { name: 'アップルパイ', price: '520' },
   { name: 'ホットサンド', price: '410' }
 ].freeze
@@ -20,7 +19,7 @@ def take_order(menus)
   end
   print '>'
   order_number = gets.to_i
-  puts "#{menus[order_number][:name]}(#{menus[order_number][:price]}円)ですね。"
+  puts "#{menus[order_number -1][:name]}(#{menus[order_number -1][:price]}円)ですね。"
   order_number
 end
 
@@ -28,7 +27,7 @@ puts 'bugカフェへようこそ！ご注文は？ 番号でどうぞ'
 order1 = take_order(DRINKS)
 
 puts 'フードメニューはいかがですか?'
-order2 = take_order(FOODS)
 
-total = FOODS[order1][:price] + DRINKS[order2][:price]
+order2 = take_order(FOODS)
+total = FOODS[order2 -1][:price].to_i+ DRINKS[order1 -1][:price].to_i
 puts "お会計は#{total}円になります。ありがとうございました！"
